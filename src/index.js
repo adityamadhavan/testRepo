6860655a99796896f06aad7a01ef7130f4e0f56f
PSIOG\aditya.madhavan <aditya.madhavan@psiog.com>
2018-08-09 12:57:33 +0530

    changes1

--- src/index.js
+++ src/index.js
@@ -2,40 +2,51 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-
 // import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 const suit = ["Spades", "Diamonds", "Hearts", "Clubs"];
 const rank = [2,3,4,5,6,7,8,9,10,11,12,13,100];
+var z = 0;
+var varPicture = [];
 var newDeckGeneral = [], createDeckArray = [],  deck = [];
 var handWithDups1 = [], handWithDups2 = [], handWithDups3 = [];
 var hand1 = [], hand2 = [], hand3 = [];
 
-var card = {
-    suit: [],
-    rank: [],
-    image: this.image
+var newCard = {
+    suit: this.suit,
+    rank: this.rank,
+    picture: this.picture
 };
+
+function pic(x, y){
+    return("/Users/adityamadhavan/cardgame/src/cards/png/" + x + "/" + y + ".png");
+}
+
+for (var x = 0; x < 4; x++){
+    for (var y = 0; y < 13; y++){
+        varPicture[z] = pic(x,y);
+        z++;
+    }
+}
+const picture = varPicture;
 
 function CreateDeck(){
     var deck = [];
+    var pictureIdx = 0;
     for (var suitIdx = 0; suitIdx < suit.length; suitIdx++) {
         for (var rankIdx = 0; rankIdx < rank.length; rankIdx++) {
-            card = {
+            newCard = {
                 suit: suit[suitIdx],
                 rank: rank[rankIdx],
-                if(suitIdx = 0){
-                    image: './cards/spades/' + rankIdx + '.png'
-                }
-                
+                picture: picture[pictureIdx]      
             };
-        deck.push(card);
+        deck.push(newCard);
+        pictureIdx++;
         }
     }
   return deck;
 }
-
 
 
 createDeckArray = CreateDeck();
@@ -54,7 +65,7 @@ newDeckGeneral = shuffleDeck(createDeckArray);
 newDeckGeneral.pop();
 deck = newDeckGeneral;
 
-// for( let i = 0; i < deck.length; i++){console.log(deck[i]);}
+// for( let i = 0; i < deck.length; i++){console.log(deck[i].picture);}
 
 function distributeCard(hand) {
     for (var i = 0; i < 17; i++) {
@@ -63,6 +74,7 @@ function distributeCard(hand) {
     }
     return hand;
 }
+deck = newDeckGeneral;
 
 handWithDups1 = distributeCard(handWithDups1);  
 handWithDups2 = distributeCard(handWithDups2);
@@ -88,47 +100,78 @@ hand1 = duplicate(handWithDups1); //Duplicates removed
 hand2 = duplicate(handWithDups2); //Duplicates removed  
 hand3 = duplicate(handWithDups3); //Duplicates removed
 
+function turn(handA, handB) { //Turn
+    let x = Math.trunc(Math.random() * handB.length);
+    let a = handB[x];
+    console.log("Selected Card")
+    console.log(handB[x]);
+    handA.push(a);
+    handB.splice(x, 1);
+    handA = duplicate(handA);
+    console.log("Hand taken by");
+    for (var i = 0; i < handA.length; i++) {
+      console.log(handA[i]);
+    }
+    console.log("Hand taken from");
+    for (var i = 0; i < handB.length; i++) {
+      console.log(handB[i]);
+    }
+    return handA;
+}
 
-export class PrintDeck extends React.Component{
+const Turn = () => {
+    return(
+        <div>
+            ...
+        </div>    
+    );
+}
+
+class Card extends React.Component{
     render(){
         return(
             <div>
-                <ul> 
-                    {deck.map((deck, i) => <li key={i}>{deck.image}</li>)}
-                </ul> 
-            </div>       
-        );
-    }    
-};
-
-export class Hand extends React.Component{
-    render(){
-        return(
-            <div>
-                <div className="container">
-                    <div className="row">
-                        <div className="col-sm-4">
-                            <ul> 
-                                {hand1.map((hand1, i) => <li key={i}>{hand1.rank} of {hand1.suit}</li>)}
-                            </ul> 
-                        </div>
-                        <div className="col-sm-4">
-                            <ul> 
-                                {hand2.map((hand2, i) => <li key={i}>{hand2.rank} of {hand2.suit}</li>)}
-                            </ul>
-                        </div>
-                        <div className="col-sm-4">
-                            <ul> 
-                                {hand3.map((hand3, i) => <li key={i}>{hand3.rank} of {hand3.suit}</li>)}
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>        
+                <img width="30" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfkzqDnXL_gZ2SsOZBxXd2Yh3IpJpTTorbiEqVBtgDgPZbnxHH" />
+                <p>{this.props.rank} of {this.props.suit}</p>
+            </div>
         );
     }
 }
 
-ReactDOM.render(<Hand />, document.getElementById('root'));
-registerServiceWorker();
+class Hand extends React.Component{
+    render(){
+        return(
+            <div>
+                {this.props.card.map((card) => <Card {...card} />)}
+            </div>
+        );
+    }
+}
 
+class Board extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>Welcome to Old Boy</h1>
+                <button onClick={() => alert('Refresh Page to Start New Game')}>New Game</button>
+                <hr/>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-sm-4">
+                        <Hand card={hand1}/>
+                        </div>
+                        <div className="col-sm-4">
+                            <Hand card={hand2}/>
+                        </div>
+                        <div className="col-sm-4">
+                            <Hand card={hand3}/>
+                        </div>
+                    </div>
+                </div>    
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<Board />, document.getElementById('root'));
+registerServiceWorker();
